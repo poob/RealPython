7eeaf33767e150bb0af50114bd5a90218e84e538
U-PoobComp\Poob <Poob@PoobComp.(none)>
2014-06-08 16:13:23 -0500

    First creation of repo

---
+++ Chapter 1/hello_world.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 29, 2012
+
+@author: Poob
+'''
+
+# Normal print
+print "Hello, world"
+phrase = "Hello, world"
+print phrase
+print "phrase"
---
+++ Chapter 2/StringsAndMethods.py
@@ -0,0 +1,21 @@
+'''
+Created on Nov 20, 2012
+
+@author: Poob
+'''
+
+# Example of string literals
+phrase = 'Hello, world.'
+myString = "We're #1!"
+stringNumber = "1234"
+
+# Example string with nested quotes
+conversation = 'I said, "Put it over by the llama."'
+
+# Example string span many lines in source code
+longString = '''This is a string
+that spans across multiple lines in the source code'''
+
+longString = """This is a new string
+that spans across two lines"""
+
